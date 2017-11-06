50a84c644d367ea8766dda7f5ed275b7eecbdcff
Alex Rodriguez <alerodriguez@csumb.edu>
2017-11-06 17:36:01 +0000

    Socket.io integration v1.0, Landing Page updated, login and signup page in progress stay tuned

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+var path = require('path');
+
+//include routes to views
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var app = express();
-
-// view engine setup
+//make public folder public
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.get('*', function(req, res, next) {
+  var err = new Error();
   err.status = 404;
   next(err);
 });
 
-// error handler
+// handling 404 errors
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if (err.status !== 404) {
+    return next();
+  }
+  res.send(err.message || '404 Page Not Found');
 });
 
-module.exports = app;
+
+//displays port being used 
+http.listen(process.env.PORT, function() {
+  console.log(process.env.IP + ":" + process.env.PORT);
+});
+
+
+io.on('connection', function(socket) {
+  // io.emit({'msg': 'test'})
+  io.emit('chat message',"Socket.io test");
+  // console.log('a user connected');
+  // socket.on('chat message', function(msg) {
+  //   io.emit('chat message', msg);
+  // });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// var greetings = require("./app.js");
+
+// console.log(greetings.getIP())
+
+// var socket = io.connect('http://'+process.env.IP+'8080/');
+// socket.on('chat message', function(msg) {
+    
+//     alert(msg)
+// });
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Common Sync' });
+  res.render('index', { title: 'CommonSync' ,
+                        title2: 'Hi'});
+
 });
 
 module.exports = router;
