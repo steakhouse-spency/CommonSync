5d715cc32a916d617e7ccb2be3d8c3d8b1047e69
Alex Rodriguez <alerodriguez@csumb.edu>
2017-12-05 08:18:41 +0000

    home page dropdown code re-added, fixed sidebar issue

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var path = require('path');
-
+var path = require('path');    
 
 //include routes to views
 console.log("Setting up routes...");
