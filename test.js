fdeb5f04e5e5f7d5c39b92f20259b8f66e08c833
Jesus Perez <jesuperez@csumb.edu>
2017-12-13 02:32:33 +0000

    testing

--- test.js
+++ test.js
@@ -1,8 +1,31 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
-    });
-  });
-});
+var firebase = require("firebase");
+
+
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAMHYGpu-H8_QgL2_XJJlZldZyOXHRu3Do",
+    authDomain: "common-sync.firebaseapp.com",
+    databaseURL: "https://common-sync.firebaseio.com",
+    projectId: "common-sync",
+    storageBucket: "common-sync.appspot.com",
+    messagingSenderId: "225743183786"
+  };
+  firebase.initializeApp(config);
+   database = firebase.database();
+   var ref = database.ref('Users');
+   
+  
+  
+  
+  
+  
+
+// var assert = require('assert');
+// describe('Array', function() {
+//   describe('#indexOf()', function() {
+//     it('should return -1 when the value is not present', function() {
+//       assert.equal([1,2,3].indexOf(4), -1);
+//     });
+//   });
+// });
+
