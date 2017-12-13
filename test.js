1a0a6e69a367be34769933bfab5be8d29ccaab00
Jesus Perez <jesuperez@csumb.edu>
2017-12-13 02:33:43 +0000

    testing

--- test.js
+++ test.js
@@ -20,12 +20,12 @@ var firebase = require("firebase");
   
   
 
-// var assert = require('assert');
-// describe('Array', function() {
-//   describe('#indexOf()', function() {
-//     it('should return -1 when the value is not present', function() {
-//       assert.equal([1,2,3].indexOf(4), -1);
-//     });
-//   });
-// });
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1,2,3].indexOf(4), -1);
+    });
+  });
+});
 
